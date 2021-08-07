e24154ffa8386595da15a8eb097f93b9fba15a6f
huynhnhan112 <huunhan557@gmail.com>
2021-08-08 01:39:10 +0700

    react_hw

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 // import Glasses from './Glasses/Glasses'
-// import BaiTapOanTuXi from './BaiTapRedux/BaiTapOanTuXi/BaiTapOanTuXi';
-// import BaiTapGameXucXac from './BaiTapRedux/BaiTapGameXucXac/BaiTapGameXucXac';
+import BaiTapOanTuXi from './BaiTapRedux/BaiTapOanTuXi/BaiTapOanTuXi';
+import BaiTapGameXucXac from './BaiTapRedux/BaiTapGameXucXac/BaiTapGameXucXac';
 import BaiTapBookingTicket from './BaiTapRedux/BaiTapBookingTicket/BaiTapBookingTicket';
 
 function App() {
   return (
     <div className="App">
       {/* <Glasses /> */}
-      {/* <BaiTapOanTuXi /> */}
-      {/* <BaiTapGameXucXac /> */}
+      <BaiTapOanTuXi />
+      <BaiTapGameXucXac />
       <BaiTapBookingTicket />
     </div>
   );
